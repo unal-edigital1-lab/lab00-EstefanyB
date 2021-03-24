8fc4d63b47f081c9077351a49b0e3b0a2bef2c26
EstefanyB <ebautista@unal.edu.co>
2021-03-24 10:20:22 -0500

    Update sum1bcc.v

--- hdl/src/sum1bcc.v
+++ hdl/src/sum1bcc.v
@@ -1,14 +1,14 @@
 module sum1bcc (A, B, Ci,Cout,S);
-
+	//Declara entradas y salidas
   input  A;
   input  B;
   input  Ci;
   output Cout;
   output S;
 
-  reg [1:0] st;
-  assign S = st[0];
-  assign Cout = st[1];
+  reg [1:0] st; //st es un arreglo de dos bits que almacena información
+  assign S = st[0]; // se le asgina la posición 0 de st
+  assign Cout = st[1]; // se le asgina la posición 1 de st
 
   always @ ( * ) begin
   	st  = 	A+B+Ci;
