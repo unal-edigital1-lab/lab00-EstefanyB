81aece151d911f8bcc8b69beafcf1f70e25edb54
EstefanyB <ebautista@unal.edu.co>
2021-03-24 10:21:51 -0500

    Update sum1bcc_TB.v

--- hdl/src/sum1bcc_TB.v
+++ hdl/src/sum1bcc_TB.v
@@ -1,17 +1,17 @@
 module sum1bcc_TB;
 
-  reg x;
-  reg y;
+  reg x; //reg son arreglos de almacenamiento
+  reg y; // wire son cables de conexión
   reg c;
   wire out;
   wire z;
 
 
-sum1bcc uut(x, y, c,out,z);
+sum1bcc uut(x, y, c,out,z); //Instanciar
 
 
 initial begin
-x=0; y=0; c=0; #3;
+x=0; y=0; c=0; #3; // #3: indica tiempo de espera en ciclos
 x=0; y=0; c=1; #3;
 x=0; y=1; c=0; #3;
 x=0; y=1; c=1; #3;
@@ -27,7 +27,7 @@ end
 initial begin: TEST_CASE
      $dumpfile("sum1bcc_TB.vcd");
      $dumpvars(-1, uut);
-     #(200) $finish;
+     #(200) $finish; //Tiempo de finalización
    end
 
 endmodule //
